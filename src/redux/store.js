056495fa973888cb430dad60d7334d06a26660ce
vitalij99 <vitalisklymko@gmail.com>
2023-04-15 21:49:58 +0300

    fix Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { reducer } from './reducer';
 
@@ -9,5 +19,13 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
-export const store = configureStore({ reducer });
-// export const persistor = persistStore(store);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
